82b1eb3d677dfa0d5cc5af35b5ccdbf5229e375f
Gopichand <109204175+gopichandpantala@users.noreply.github.com>
2023-07-04 16:12:26 +0530

    Create 22.04_install_mdatp.sh
    For Ubuntu 22.04

---
+++ 22.04_install_mdatp.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Install curl
+sudo apt-get install curl
+
+# Install libplist-utils
+sudo apt-get install libplist-utils
+
+# Download Microsoft repository configuration
+curl -o microsoft.list https://packages.microsoft.com/config/ubuntu/22.04/prod.list
+sudo mv ./microsoft.list /etc/apt/sources.list.d/microsoft-prod.list
+
+# Install gpg and gnupg
+sudo apt-get install gpg
+sudo apt-get install gnupg
+
+# Import Microsoft GPG key
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+
+# Install apt-transport-https
+sudo apt-get install apt-transport-https
+
+# Update package lists
+sudo apt-get update
+
+# Install Microsoft Defender ATP (mdatp)
+sudo apt-get install mdatp
+
+# Display contents of /etc/apt/sources.list.d/
+cat /etc/apt/sources.list.d/*
+
+# Install mdatp with the Jammy repository target
+sudo apt -t jammy install mdatp
